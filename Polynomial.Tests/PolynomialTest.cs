a6df4a6432448a7bc9f817876fe68d604c4ca261
artyoda <artyoda05@gmail.com>
2020-05-15 13:04:31 +0300

    the prototype

---
+++ Polynomial.Tests/PolynomialTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using PolynomialObject;
+using PolynomialObject.Exceptions;
+using System;
+using System.Collections.Generic;
+
+namespace PolynomialTest
+{
+    [TestFixture]
+    public class PolynomialTest
+    {
+        //todo
+        [Test]
+        public void SimpleFalse()
+        {
+            Assert.IsTrue(false);
+        }
+        [Test]
+        public void SimpleTrue()
+        {
+           Assert.IsTrue(true);
+        }
+    }
+}
